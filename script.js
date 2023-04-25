006fdd931501d1a4b839f98efd21f02eb0783d43
Niraj Patil <112927422+nirajpatil22400@users.noreply.github.com>
2023-04-26 00:21:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// your code here
+    const form = document.querySelector('form');
+    const button = document.querySelector('#button');
+    const url = document.querySelector('#url');
+
+    form.addEventListener('submit', (event) => {
+      event.preventDefault(); // prevent form from submitting
+
+      const name = document.querySelector('#name').value;
+      const year = document.querySelector('#year').value;
+
+      // build the query string
+      const queryString = `?name=${name}&year=${year}`;
+
+      // update the URL text
+      url.textContent = `https://localhost:8080/${queryString}`;
+    });
+  
